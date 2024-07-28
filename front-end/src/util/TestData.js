e25f34e19682d1fddacdbfd914edf50bf3f3906f
JinY <wlsdud6221@naver.com>
2024-07-29 03:26:23 +0900

    init - 테스트데이터 작성

---
+++ front-end/src/util/TestData.js
@@ -0,0 +1,41 @@
+export const FitnessData = [
+  {
+    title: '벤치프레스',
+    src: '',
+  },
+  {
+    title: '데드리프트',
+    src: '',
+  },
+  {
+    title: '스쿼트',
+    src: '',
+  },
+  {
+    title: '벤치프레스',
+    src: '',
+  },
+  {
+    title: '벤치프레스',
+    src: '',
+  },
+  {
+    title: '벤치프레스',
+    src: '',
+  },
+  {
+    title: '벤치프레스',
+    src: '',
+  },
+];
+
+export const FitnessPlanData = [
+  {
+    title: '벤치프레스',
+    plan: [],
+  },
+  {
+    title: '스쿼트',
+    plan: [],
+  },
+];

65878cc9d12adb93e618c5073b71cc772759fd32
JinY <wlsdud6221@naver.com>
2024-07-29 17:56:32 +0900

    init - test 데이터 추가

--- front-end/src/util/TestData.js
+++ front-end/src/util/TestData.js
@@ -29,13 +29,92 @@ export const FitnessData = [
   },
 ];
 
-export const FitnessPlanData = [
-  {
-    title: '벤치프레스',
-    plan: [],
-  },
-  {
-    title: '스쿼트',
-    plan: [],
-  },
-];
+export const FitnessPlanData = {
+  title: '새루틴1',
+  due_date: '2024-08-15',
+  exercise: [
+    {
+      id: 1,
+      name: '벤치프레스',
+      detail: [
+        {
+          id: 1,
+          set: 1,
+          weight: 50,
+          count: 15,
+          is_complete: true,
+        },
+        {
+          id: 2,
+          set: 2,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+        {
+          id: 2,
+          set: 3,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: '스쿼트',
+      detail: [
+        {
+          id: 1,
+          set: 1,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+        {
+          id: 2,
+          set: 2,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+        {
+          id: 2,
+          set: 3,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: '데드리프트',
+      detail: [
+        {
+          id: 1,
+          set: 1,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+        {
+          id: 2,
+          set: 2,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+        {
+          id: 2,
+          set: 3,
+          weight: 0,
+          count: 15,
+          is_complete: false,
+        },
+      ],
+    },
+  ],
+};
+
+export const exerciseData = [600, 700, 550, 340, 580, 620];

8bec8ab63c485851e2c6dec36f1db2787020ef8c
JinY <wlsdud6221@naver.com>
2024-07-29 17:56:18 +0900

    design - Header UI 변경

--- front-end/src/components/Common/Header.tsx
+++ front-end/src/components/Common/Header.tsx
@@ -8,10 +8,10 @@ const s = {
   Container: styled.section`
     width: 100%;
     height: 57px;
-    border: 1px solid red;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    background-color: ${(props) => props.theme.bgColor};
   `,
   BasicArea: styled.div`
     width: 200px;
@@ -19,7 +19,6 @@ const s = {
     margin: 0 10px;
     display: flex;
     align-items: center;
-    border: 1px solid yellow;
   `,
   Title: styled.span`
     color: ${(props) => props.theme.textColor};
@@ -29,14 +28,19 @@ const s = {
   `,
 };
 
-const Header = (): JSX.Element => {
+interface HeaderProps {
+  text?: string;
+  children?: React.ReactNode;
+}
+
+const Header = (props: HeaderProps): JSX.Element => {
   return (
     <s.Container>
       <s.BasicArea>
         <IconSvg width="25" height="25" Ico={back} />
-        <s.Title>테스트</s.Title>
+        <s.Title>{props.text}</s.Title>
       </s.BasicArea>
-      <Button width="80px" height="40px" children="불러오기" onClick={() => {}} size="14px" margin="0 20px 0 0" />
+      {props.children}
     </s.Container>
   );
 };

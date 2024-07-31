0d114dd97c9e552611f43335946c492e63e4ee46
Park SangCheon <pswlove38@naver.com>
2024-07-31 17:33:31 +0900

    feat-psc-Add Redis Test

---
+++ back-end/src/main/java/com/ssafy/back_end/redis/service/RedisService.java
@@ -0,0 +1,28 @@
+package com.ssafy.back_end.redis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object findByKey(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteByKey(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean existsByKey(String key) {
+        return redisTemplate.hasKey(key);
+    }
+}

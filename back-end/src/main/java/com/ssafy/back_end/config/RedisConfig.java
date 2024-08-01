a2e26286279223976648c60c236e39dcc85aac98
Park SangCheon <pswlove38@naver.com>
2024-08-01 10:18:43 +0900

    feat-psc-Add Redis

--- back-end/src/main/java/com/ssafy/back_end/config/RedisConfig.java
+++ back-end/src/main/java/com/ssafy/back_end/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -13,8 +14,16 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
+
+        // keySerializer는 StringRedisSerializer를 사용
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+
+        // valueSerializer는 Jackson2JsonRedisSerializer를 사용
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        // 기본 설정으로 다양한 타입 지원
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
         return template;
     }
 }

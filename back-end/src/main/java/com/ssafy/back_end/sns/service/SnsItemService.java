ded33616829e936675ff4c9d8b6b8a39e4895c61
Choi yonghun <qqa109@naver.com>
2024-08-03 22:22:21 +0900

    feat-cyh-sns-item구현중

--- back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemService.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemService.java
@@ -1,4 +1,20 @@
 package com.ssafy.back_end.sns.service;
 
+import com.ssafy.back_end.sns.model.ItemDto;
+
+import java.util.List;
+
 public interface SnsItemService {
+
+    int writeItem(ItemDto item, List<String> images);   //중고장터 작성
+
+    int updateItem(ItemDto item, List<String> images);   //중고장터 수정
+
+    int deleteItem(int itemId);   //중고장터 삭제
+
+    int isLike(int itemId, int userId);   //내가 좋아요 눌렀는지 확 인
+
+    int likeItem(int itemId, int userId);   //중고장터 좋아요
+
+    int dislikeItem(int itemId, int userId);   //중고장터 좋아요 취소
 }
--- back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemServiceImpl.java
+++ back-end/src/main/java/com/ssafy/back_end/sns/service/SnsItemServiceImpl.java
@@ -1,4 +1,74 @@
 package com.ssafy.back_end.sns.service;
 
-public class SnsItemServiceImpl {
+import com.ssafy.back_end.sns.mapper.SnsItemMapper;
+import com.ssafy.back_end.sns.model.ItemDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SnsItemServiceImpl implements SnsItemService {
+
+    private final SnsItemMapper snsItemMapper;
+
+    @Autowired
+    public SnsItemServiceImpl(SnsItemMapper snsItemMapper) {
+        this.snsItemMapper = snsItemMapper;
+    }
+
+    @Override
+    @Transactional
+    public int writeItem(ItemDto item, List<String> images) {
+        snsItemMapper.insertItem(item);
+
+        if (!images.isEmpty()) {
+            snsItemMapper.insertItemImages(item.getId(), images);
+        }
+
+        return item.getId();
+    }
+
+    @Override
+    @Transactional
+    public int updateItem(ItemDto item, List<String> images) {
+
+//        SET title = #{title}, price = #{price}, content = #{content}, updated_at = NOW()
+//        WHERE id = #{id};
+//
+//        FeedInteractionDto comment = FeedInteractionDto.builder()
+//                .feedId(feedInteractionDto.getFeedId())
+//                .userId(feedInteractionDto.getUserId())
+//                .comment(feedInteractionDto.getComment())
+//                .build();
+        snsItemMapper.updateItem(item);
+        snsItemMapper.deleteItemImages(item.getId());
+
+        if (!images.isEmpty()) {
+            snsItemMapper.insertItemImages(item.getId(), images);
+        }
+
+        return item.getId();
+    }
+
+    @Override
+    public int deleteItem(int itemId) {
+        return snsItemMapper.deleteItem(itemId);
+    }
+
+    @Override
+    public int isLike(int itemId, int userId) {
+        return snsItemMapper.isLike(itemId, userId);
+    }
+
+    @Override
+    public int likeItem(int itemId, int userId) {
+        return snsItemMapper.likeItem(itemId, userId);
+    }
+
+    @Override
+    public int dislikeItem(int itemId, int userId) {
+        return snsItemMapper.dislikeItem(itemId, userId);
+    }
 }

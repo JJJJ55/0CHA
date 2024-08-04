a62c8b85a8b079624edb2fa88790a0ed403af207
JinY <wlsdud6221@naver.com>
2024-08-05 03:30:27 +0900

    feat - ojy - sns 관련 라우팅 설정

--- front-end/src/components/SNS/CommentModal.tsx
+++ front-end/src/components/SNS/CommentModal.tsx
@@ -9,26 +9,22 @@ import { useState } from 'react';
 
 import test from '../../asset/img/testImg.png';
 
-
 const s = {
   Container: styled.section`
-    width: 100%;
+    height: 100%;
+    padding: 10px;
+    background-color: ${(props) => props.theme.bgColor};
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0px 15px;
+    flex-direction: column;
+    border-radius: 10px 10px 0 0;
   `,
   CreateComment: styled.section`
     display: flex;
     justify-content: space-between;
   `,
-  ReactModal: styled(ReactModal)`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    `,
   CommentArea: styled.div`
+    flex-grow: 1;
+    overflow: auto;
   `,
   TopBar: styled.div`
     margin: 0 auto;
@@ -36,81 +32,97 @@ const s = {
     height: 3px;
     background-color: #ffffff;
     margin-bottom: 25px;
+    cursor: pointer;
   `,
   CreateButton: styled.div`
     margin-left: 10px;
-  `
+  `,
 };
 
+interface CommentModalProps {
+  open: boolean;
+  onModal: Function;
+}
 
-const CommentModal = (): JSX.Element => {
-  const [modalIsOpen, setModalIsOpen] = useState(true);
+const CommentModal = (props: CommentModalProps): JSX.Element => {
   const toggleModal = () => {
-    setModalIsOpen(false);
-  }
+    props.onModal();
+  };
   return (
     <>
-      <s.ReactModal
-        isOpen={modalIsOpen}
+      <ReactModal
+        isOpen={props.open}
         ariaHideApp={false}
-        shouldCloseOnOverlayClick={true}
-        shouldCloseOnEsc={true}
-        onRequestClose={toggleModal}
-        style={{
-          overlay: {
-            width: "100%",
-            maxWidth: "800px",
-            top: "30%",
-            margin: "0 auto",
-            backgroundColor: "#000000",
-            borderRadius: "12px 12px 0px 0px",
-          },
-          content: {
-            padding: "25px",
-            paddingTop: "10px",
-            color: "#ffffff",
-          }
-        }}
+        onRequestClose={() => props.onModal()}
+        overlayClassName="Overlay"
+        className="CommentModal"
       >
-      <s.CommentArea>
-        <s.TopBar onClick={toggleModal}></s.TopBar>
-        <Comment
-          commentProfileImage={test}
-          commentAuthor='stranger_00'
-          commentContent='그 루틴 좋아보이네여'
-          isUserComment={false}
-        />
-        <Comment
-          commentProfileImage={test}
-          commentAuthor='stranger_00'
-          commentContent='아아 마이크체크 마이크체크'
-          isUserComment={false}
-        />
-        <Comment
-          commentProfileImage={test}
-          commentAuthor='it_is_me'
-          commentContent='내 댓글이예요'
-          isUserComment={true}
-        />
-      </s.CommentArea>
-      <s.CreateComment>
-        <Input
-          width="100%"
-          height="40px"
-          placeholder="댓글 작성"
-        />
-        <s.CreateButton>
-          <Button
-            width="64px"
-            height="40px"
-            type="main"
-            children="작성"
-            size="14px"
-            bold="500"
-          />
-        </s.CreateButton>
-      </s.CreateComment>
-      </s.ReactModal>
+        <s.Container>
+          <s.TopBar onClick={toggleModal}></s.TopBar>
+          <s.CommentArea>
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="stranger_00"
+              commentContent="그 루틴 좋아보이네여"
+              isUserComment={false}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="stranger_00"
+              commentContent="아아 마이크체크 마이크체크"
+              isUserComment={false}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+            <Comment
+              commentProfileImage={test}
+              commentAuthor="it_is_me"
+              commentContent="내 댓글이예요"
+              isUserComment={true}
+            />
+          </s.CommentArea>
+          <s.CreateComment>
+            <Input width="100%" height="40px" placeholder="댓글 작성" />
+            <s.CreateButton>
+              <Button width="64px" height="40px" type="main" children="작성" size="14px" bold="500" />
+            </s.CreateButton>
+          </s.CreateComment>
+        </s.Container>
+      </ReactModal>
     </>
   );
 };
--- front-end/src/pages/SocialPage/Feed/CreateFeedPage.tsx
+++ front-end/src/pages/SocialPage/Feed/CreateFeedPage.tsx
@@ -7,8 +7,8 @@ import TextArea from '../../../components/Common/TextArea';
 import Button from '../../../components/Common/Button';
 import Image from '../../../components/Common/Image';
 
-import test from "../../../asset/img/testImg.png";
-
+import test from '../../../asset/img/testImg.png';
+import { useNavigate } from 'react-router';
 
 const s = {
   Container: styled.section`
@@ -37,69 +37,51 @@ const s = {
     margin: 15px 0;
   `,
   Button: styled.div`
-  display: flex;
-  width: 100%;
-  max-width: 800px;
-  padding: 0 25px;
-  position: fixed;
-  bottom: 88px;
-`,
-
+    display: flex;
+    width: 100%;
+    max-width: 800px;
+    padding: 0 25px;
+    position: fixed;
+    bottom: 88px;
+  `,
 };
 
-
 const CreateFeedPage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const handleMovePage = (): void => {
+    navigate('/sns');
+  };
   return (
     <>
-    <Header text="운동 피드 작성"/>
-    <s.Container>
-      <s.ImageArea>
-        <Image
-          width="100%"
-          height="auto"
-          src={test}
-          type="rect"
-        />
-        <s.RoutineArea>
-          <s.Routine>testtext1</s.Routine>
-          <s.Routine>testtext2</s.Routine>
-          <s.Routine>testtext3</s.Routine>
-        </s.RoutineArea>
-      </s.ImageArea>
-      
+      <Header text="운동 피드 작성" />
+      <s.Container>
+        <s.ImageArea>
+          <Image width="100%" height="auto" src={test} type="rect" />
+          <s.RoutineArea>
+            <s.Routine>testtext1</s.Routine>
+            <s.Routine>testtext2</s.Routine>
+            <s.Routine>testtext3</s.Routine>
+          </s.RoutineArea>
+        </s.ImageArea>
 
-      <s.SelectPicture>
+        <s.SelectPicture>
+          <Button width="49%" height="40px" size="14px" bold="500" children="사진 촬영" />
+          <Button width="49%" height="40px" size="14px" bold="500" children="갤러리" />
+        </s.SelectPicture>
+        <TextArea width="100%" height="180px" />
+      </s.Container>
+      <s.Button>
         <Button
-          width="49%"
+          width="100%"
           height="40px"
           size="14px"
+          type="main"
           bold="500"
-          children="사진 촬영"
+          children="작성완료"
+          onClick={handleMovePage}
         />
-        <Button
-          width="49%"
-          height="40px"
-          size="14px"
-          bold="500"
-          children="갤러리"
-        />
-      </s.SelectPicture>
-      <TextArea
-        width="100%"
-        height="180px"
-      />
-    </s.Container>
-    <s.Button>
-      <Button
-        width="100%"
-        height="40px"
-        size="14px"
-        type="main"
-        bold="500"
-        children="작성완료"
-      />
-    </s.Button>
-    <BottomNav/>
+      </s.Button>
+      <BottomNav />
     </>
   );
 };

0b7baa9e988e233a43267359272f56e44cfe6800
김고은 <kke0402@naver.com>
2024-08-05 17:42:15 +0900

    feat-kke-피드 axios 요청

--- front-end/src/pages/SocialPage/Feed/FeedPage.tsx
+++ front-end/src/pages/SocialPage/Feed/FeedPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import Feed from '../../../components/SNS/Feed';
@@ -13,6 +13,10 @@ import { useAppDispatch, useAppSelector } from '../../../lib/hook/useReduxHook';
 import { modalActions, selectModalComment, selectModalUserSearch } from '../../../store/modal';
 import { useNavigate } from 'react-router';
 import { useModalExitHook } from '../../../lib/hook/useModalExitHook';
+
+import { FeedData } from '../../../util/FeedTestData.js';
+import FeedList from '../../../components/SNS/FeedList';
+import axios from 'axios';
 
 
 const s = {
@@ -61,10 +65,6 @@ const s = {
   `,
 };
 
-interface dataType {
-  email: string;
-  pw: string;
-}
 
 const FeedPage = (): JSX.Element => {
   const navigate = useNavigate();
@@ -81,13 +81,30 @@ const FeedPage = (): JSX.Element => {
   const toggleModalUserSearch = (): void => {
     dispatch(modalActions.toggleUserSearch());
   };
+
+
+  const [feedData, setFeedData] = useState([]);
+  const getFeedData = async () => {
+    try {
+      const res = await axios.get('/testdata/FeedTestData.json');
+      setFeedData(res.data)
+    } catch(error) {
+      console.error(error)
+    }
+  };
+  useEffect(() => {
+    getFeedData();
+  }, []);
+
+  
   useModalExitHook();
   return (
     <>
       <s.Container>
         <SnsHeader />
         <SnsNavigation />
-        <Feed
+        <FeedList data={feedData} onClick={toggleModalComment} />
+        {/* <Feed
           width="100vw"
           height="100vw"
           src={test}
@@ -98,7 +115,7 @@ const FeedPage = (): JSX.Element => {
           commentCnt="30"
           content="example content test"
           onClick={toggleModalComment}
-        />
+        /> */}
         <CommentModal open={isComment} onModal={toggleModalComment} />
         <UserSearchModal open={isUserSearch} onModal={toggleModalUserSearch} />
       </s.Container>

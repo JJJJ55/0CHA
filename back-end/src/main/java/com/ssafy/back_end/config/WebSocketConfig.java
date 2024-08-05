6da4a51c62c074e1c7e46db146ff632e49d17893
Park SangCheon <pswlove38@naver.com>
2024-08-05 17:25:55 +0900

    feat-psc-Modify chat setting

--- back-end/src/main/java/com/ssafy/back_end/config/WebSocketConfig.java
+++ back-end/src/main/java/com/ssafy/back_end/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.ssafy.back_end.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,9 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker  // STOMP 사용
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {

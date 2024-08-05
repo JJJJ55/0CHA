38892342333cc24701f28b7cbd410c42684d5013
JinY <wlsdud6221@naver.com>
2024-08-05 16:00:29 +0900

    fix - ojy - strick 해제

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -14,14 +14,12 @@ const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <App />
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
+  </Provider>,
 );
 
 // If you want your app to work offline and load faster, you can change

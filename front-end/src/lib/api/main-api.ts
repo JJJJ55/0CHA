5833937ef96add15f6cf01185fbd19996f1a8d27
JinY <wlsdud6221@naver.com>
2024-08-07 10:33:59 +0900

    fix - ojy - RestAPI axios 세팅

--- front-end/src/lib/api/main-api.ts
+++ front-end/src/lib/api/main-api.ts
@@ -1,4 +1,6 @@
+import { AxiosError } from 'axios';
 import { localAxios, publicAxios } from '../../util/axios-setting';
+import { changePw, MyInfo, Profile } from '../../util/types/axios-main';
 
 const local = publicAxios();
 const jwt = localAxios();
@@ -6,65 +8,64 @@ const jwt = localAxios();
 // 상단 루틴가져오기
 
 // 내 루틴가져오기
-export const MyRoutine = async (
-  param: UserLogin,
-  success: (response: any) => void,
-  fail: (error: AxiosError) => void,
-) => {
-  await local.post(`/main/routines`, param).then(success).catch(fail);
+export const getMyRoutine = async (success: (response: any) => void, fail: (error: AxiosError) => void) => {
+  await jwt.get(`/main/routines`).then(success).catch(fail);
 };
 
 // 알림
-export const Notification = async (
-  param: UserLogin,
-  success: (response: any) => void,
-  fail: (error: AxiosError) => void,
-) => {
-  await local.post(`/main/notifications`, param).then(success).catch(fail);
+export const getNotification = async (success: (response: any) => void, fail: (error: AxiosError) => void) => {
+  await jwt.get(`/main/notifications`).then(success).catch(fail);
 };
 
 // 내 정보
-export const MyInfo = async (param: UserLogin, success: (response: any) => void, fail: (error: AxiosError) => void) => {
-  await local.post(`/main/user-info`, param).then(success).catch(fail);
+export const getMyInfo = async (success: (response: any) => void, fail: (error: AxiosError) => void) => {
+  await jwt.get(`/main/user-info`).then(success).catch(fail);
 };
 
 // 내 프로필 정보 수정
-export const ProfileModify = async (
-  param: UserLogin,
+export const putProfileModify = async (
+  param: Profile,
   success: (response: any) => void,
   fail: (error: AxiosError) => void,
 ) => {
-  await local.post(`/main/profile`, param).then(success).catch(fail);
+  await jwt.put(`/main/profile`, param).then(success).catch(fail);
 };
 
 // 닉네임 중복검사
-export const CheckNickname = async (
-  param: UserLogin,
+export const postCheckNickname = async (
+  nickname: string,
   success: (response: any) => void,
   fail: (error: AxiosError) => void,
 ) => {
-  await local.post(`/main/profile/check-nickname`, param).then(success).catch(fail);
+  await jwt
+    .post(`/main/profile/check-nickname`, nickname, {
+      headers: {
+        'Content-Type': 'application/text',
+      },
+    })
+    .then(success)
+    .catch(fail);
 };
 
 // 내 정보수정
-export const MyInfoModify = async (
-  param: UserLogin,
+export const putMyInfoModify = async (
+  param: MyInfo,
   success: (response: any) => void,
   fail: (error: AxiosError) => void,
 ) => {
-  await local.post(`/main/profile/info`, param).then(success).catch(fail);
+  await jwt.put(`/main/profile/info`, param).then(success).catch(fail);
 };
 
 // 비밀번호 수정
-export const ChangePw = async (
-  param: UserLogin,
+export const putChangePw = async (
+  param: changePw,
   success: (response: any) => void,
   fail: (error: AxiosError) => void,
 ) => {
-  await local.post(`/main/profile/password`, param).then(success).catch(fail);
+  await jwt.put(`/main/profile/password`, param).then(success).catch(fail);
 };
 
 // 회원탈퇴
-export const MyDel = async (param: UserLogin, success: (response: any) => void, fail: (error: AxiosError) => void) => {
-  await local.post(`/main/profile`, param).then(success).catch(fail);
+export const delMyDel = async (password: any, success: (response: any) => void, fail: (error: AxiosError) => void) => {
+  await jwt.delete(`/main/profile`, password).then(success).catch(fail);
 };

453d47d6d9a6af8910fcf15633dfbbb3e65f39be
ANSMOON <munbeumsu@gmail.com>
2024-08-07 15:02:58 +0900

    feat-moon-루틴 & 운동 관련 기본 기능 구현

--- back-end/src/main/java/com/ssafy/back_end/exercise/mapper/WorkoutRoutineMapper.java
+++ back-end/src/main/java/com/ssafy/back_end/exercise/mapper/WorkoutRoutineMapper.java
@@ -1,30 +1,36 @@
 package com.ssafy.back_end.exercise.mapper;
 
 import com.ssafy.back_end.exercise.model.RoutineDto;
+import com.ssafy.back_end.exercise.model.RoutineDetailDto;
 import com.ssafy.back_end.exercise.model.RoutineSetDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface WorkoutRoutineMapper {
-    List<RoutineDto> getAllRoutines(int userId);
-
-    RoutineDto getRoutineById(int routineId);
-
     int createRoutine(RoutineDto routineDto);
+    int createRoutineDetail(RoutineDetailDto routineDetailDto);
+    int createRoutineSet(RoutineSetDto routineSetDto);
 
     int updateRoutine(RoutineDto routineDto);
 
-    int deleteRoutine(int routineId);
+    int deleteRoutine(Map<String, Object> params);
+    int deleteRoutineDetailsByRoutineId(int routineId);
+    int deleteRoutineSetsByRoutineId(int routineId);
 
-    int addExerciseToRoutine(int routineId, int exerciseId);
+    RoutineDto getRoutineByIdAndUserId(Map<String, Object> params);
+    List<RoutineDetailDto> getRoutineDetailsByRoutineId(int routineId);
+    List<RoutineSetDto> getSetsByRoutineDetailId(int routineDetailId);
 
-    int removeExerciseFromRoutine(int routineId, int exerciseId);
+    List<RoutineDto> getAllRoutines(int userId);
+    List<RoutineDto> getAllUsersRoutines();
 
-    int addSetToExercise(RoutineSetDto routineSetDto);
+    int completeRoutine(Map<String, Object> params);
 
-    int removeSetFromExercise(int setId);
-
-    int completeRoutine(int routineId);
+    int likeRoutine(@Param("routineId") int routineId, @Param("userId") int userId);
+    int unlikeRoutine(@Param("routineId") int routineId, @Param("userId") int userId);
+    boolean isRoutineLiked(@Param("routineId") int routineId, @Param("userId") int userId);
 }

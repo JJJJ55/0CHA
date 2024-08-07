2a6f0e331b90c46db77f54b664fec77cfe56f6ff
qqa108 <qqa109@naver.com>
2024-08-07 10:11:44 +0900

    feat-cyh-main구현완료

--- back-end/src/main/java/com/ssafy/back_end/main/controller/MainController.java
+++ back-end/src/main/java/com/ssafy/back_end/main/controller/MainController.java
@@ -1,10 +1,15 @@
 package com.ssafy.back_end.main.controller;
 
+import com.ssafy.back_end.main.model.UserInfoDto;
+import com.ssafy.back_end.main.model.UserPasswordDto;
 import com.ssafy.back_end.main.service.MainService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "메인페이지")
 @RestController
@@ -17,58 +22,80 @@ public class MainController {
         this.mainService = mainService;
     }
 
-//    @Operation(summary = "유저 정보 조회")
-//    @GetMapping("/user-info")
-//    public ResponseEntity<?> getUserInfo(HttpServletRequest request) {
-//        int ID = (Integer)request.getAttribute("userId");
-//        UserInfoDto userInfoDto = mainService.getUserInfo(ID);
-//
-//        if (userInfoDto != null) {
-//            return ResponseEntity.ok(userInfoDto);
-//        }
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저 정보 조회 오류");
-//    }
-//
-//    @Operation(summary = "유저 프로필 수정")
-//    @PutMapping("/profile")
-//    public ResponseEntity<?> modifyProfile(HttpServletRequest request, @RequestBody UserInfoDto userInfoDto) {
-//        int ID = (Integer)request.getAttribute("userId");
-//        String nickname = userInfoDto.getNickname();
-//        String profileImage = userInfoDto.getProfileImage();
-//        int result = mainService.modifyProfile(nickname, profileImage, ID);
-//
-//        if (result != 0) {
-//            return ResponseEntity.ok("유저 프로필 수정 완료");
-//        }
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저 프로필 수정 오류");
-//    }
-//
-//    @Operation (summary = "닉네임 중복검사")
-//    @PostMapping ("/profile/check-nickname")
-//    public ResponseEntity<?> checkNickname(@RequestBody String nickname) {
-//        int result = mainService.checkNickname(nickname);
-//
-//        if (result <= 0) {
-//            return ResponseEntity.ok("사용가능한 닉네임입니다.");
-//        }
-//        else {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용중인 닉네임입니다.");
-//        }
-//    }
-//
-//    @Operation(summary = "회원정보 수정")
-//    @PutMapping("/profile/info")
-//    public ResponseEntity<?> modifyUserInfo(HttpServletRequest request, @RequestBody UserInfoDto userInfoDto) {
-//        int ID = (Integer)request.getAttribute("userId");
-//        userInfoDto.setId(ID);
-//        int result = mainService.modifyUserInfo(userInfoDto);
-//
-//        if (result != 0) {
-//            return ResponseEntity.ok("회원정보 수정 완료");
-//        }
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("회원정보 수정 오류");
-//    }
+    @Operation (summary = "유저 정보 조회")
+    @GetMapping ("/profile/info")
+    public ResponseEntity<?> getUserInfo(HttpServletRequest request) {
+        int ID = (Integer)request.getAttribute("userId");
+        UserInfoDto userInfoDto = mainService.getUserInfo(ID);
+
+        if (userInfoDto != null) {
+            return ResponseEntity.ok(userInfoDto);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저 정보 조회 오류");
+    }
+
+    @Operation(summary = "유저 프로필 수정")
+    @PutMapping ("/profile")
+    public ResponseEntity<?> modifyProfile(HttpServletRequest request, @RequestBody UserInfoDto userInfoDto) {
+        int ID = (Integer)request.getAttribute("userId");
+        String nickname = userInfoDto.getNickname();
+        String profileImage = userInfoDto.getProfileImage();
+        int result = mainService.modifyProfile(nickname, profileImage, ID);
+
+        if (result != 0) {
+            return ResponseEntity.ok("유저 프로필 수정 완료");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저 프로필 수정 오류");
+    }
+
+    @Operation (summary = "닉네임 중복검사")
+    @PostMapping ("/profile/check-nickname")
+    public ResponseEntity<?> checkNickname(@RequestBody String nickname) {
+        int result = mainService.checkNickname(nickname);
+
+        if (result <= 0) {
+            return ResponseEntity.ok("사용가능한 닉네임입니다.");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("사용중인 닉네임입니다.");
+        }
+    }
+
+    @Operation(summary = "회원정보 수정")
+    @PutMapping("/profile/info")
+    public ResponseEntity<?> modifyUserInfo(HttpServletRequest request, @RequestBody UserInfoDto userInfoDto) {
+        int ID = (Integer)request.getAttribute("userId");
+        userInfoDto.setId(ID);
+        int result = mainService.modifyUserInfo(userInfoDto);
+
+        if (result != 0) {
+            return ResponseEntity.ok("회원정보 수정 완료");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("회원정보 수정 오류");
+    }
+
+    @Operation(summary = "패스워드 변경")
+    @PutMapping("/profile/password")
+    public ResponseEntity<?> modifyPassword(HttpServletRequest request, @RequestBody UserPasswordDto userPasswordDto) {
+        int ID = (Integer)request.getAttribute("userId");
+        int result = mainService.modifyPassword(ID, userPasswordDto.getCurPassword(), userPasswordDto.getNewPassword());
+
+        if (result != 0) {
+            return ResponseEntity.ok("패스워드 변경 완료");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("패스워드 변경 오류");
+    }
+
+    @Operation(summary = "회원탈퇴")
+    @DeleteMapping("/profile")
+    public ResponseEntity<?> deleteUser(HttpServletRequest request, @RequestBody String password) {
+        int ID = (Integer)request.getAttribute("userId");
+        int result = mainService.deleteUser(ID, password);
+        System.out.println(ID + " " + password + " " + result);
+
+        if (result != 0) {
+            return ResponseEntity.ok("회원탈퇴 완료");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("회원탈퇴 오류");
+    }
 }
-//int modifyPassword(int id, String curPassword, String newPassword);   //패스워드 변경
-//
-//int deleteUser(int id, String password);   //비밀번호 변경

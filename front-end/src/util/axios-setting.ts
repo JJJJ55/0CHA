3eeaa55066937720e2bc1af101a0b167b86a5598
JinY <wlsdud6221@naver.com>
2024-08-15 04:32:09 +0900

    fix - ojy - FE배포

--- front-end/src/util/axios-setting.ts
+++ front-end/src/util/axios-setting.ts
@@ -4,7 +4,7 @@ import { getAccessToken, getRefreshToken, removeTokens, setAccessToken } from '.
 
 export const localAxios = () => {
   const instance: Axios = axios.create({
-    baseURL: '/proxy',
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
   });
 
@@ -172,7 +172,7 @@ export const localAxios = () => {
 
 export const publicAxios = () => {
   const publicAxios: Axios = axios.create({
-    baseURL: '/proxy',
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
   });
 
@@ -184,7 +184,7 @@ export const publicAxios = () => {
 
 export const wsAxios = () => {
   const instance: Axios = axios.create({
-    baseURL: '/proxy',
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
   });
 
@@ -278,7 +278,7 @@ export const wsAxios = () => {
 
 export const socialAxios = () => {
   const publicAxios: Axios = axios.create({
-    baseURL: '/social',
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
   });
 
